a258ce78701861ce63555162003911dc9b2ca32e
Braxton Gunter <braxton.gun@gmail.com>
2017-12-03 10:43:17 -0500

    interim edits -- need free parser

--- newbie.ml
+++ newbie.ml
@@ -1,6 +1,6 @@
 (* top-level of newbie compiler *)
 
-type actions = TOKEN  | AST | (* SAST | LLVIM_IR | COMPILE | *)  DEFAULT
+type actions = TOKEN  | AST | SAST | LLVIM_IR | COMPILE | DEFAULT
 
 let main () =
   let is_tag str =
@@ -11,11 +11,9 @@ let main () =
       List.assoc Sys.argv.(1) [
           ("-t", TOKEN)     ; (* output tokens only *)
           ("-a", AST)       ; (* output ast only*)
-        (* 
           ("-s", SAST)      ; (* output sast only *)
           ("-l", LLVIM_IR)  ; (* generate, do NOT check *)
           ("-c", COMPILE)     (* generate, check LLVM IR *)
-         *)
       ]
     else if (Array.length Sys.argv = 2 && not (is_tag Sys.argv.(1))) then       (* no tag *)
       DEFAULT
@@ -39,16 +37,17 @@ let main () =
         | []      -> failwith "sheesh!"
   in
   let gen_ast = Parser.program cache lexbuf in
-  (* let gen_sast = Semant.check gen_ast in *)
+  let gen_sast = Semant.check gen_ast in
   match action with
       TOKEN           -> print_endline (Scanner.string_of_tokens tokens)
     | AST             -> print_endline (Ast.string_of_program gen_ast)
-    (* | SAST            -> print_endline (Sast.string_of_program gen_sast) *)
-    (* | LLVIM_IR        -> print_endline (Llvm.string_of_llmodule (Codegen.translate gen_ast)) (* TODO: make gen_sast *) *)
-    (* | COMPILE         -> let m = Codegen.translate gen_ast in
+    | SAST            -> print_endline (Sast.string_of_program gen_sast)
+    | LLVIM_IR        -> print_endline (Llvm.string_of_llmodule (Codegen.translate gen_sast))
+    | COMPILE         -> let m = Codegen.translate gen_ast in
          Llvm_analysis.assert_valid_module m; print_string (Llvm.string_of_llmodule m) *)
     | DEFAULT         ->  print_endline (Scanner.string_of_tokens tokens) ; 
                           print_endline (Ast.string_of_program gen_ast) ;
+                          print_endline (Sast.string_of_sprogam gen_sast) ;
                           print_endline (Llvm.string_of_llmodule (Codegen.translate gen_ast))
 
 let _ = Printexc.print main ()
