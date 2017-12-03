3a2962cbff0ea3a3d38925ad57277c40a8c4436e
Braxton Gunter <braxton.gun@gmail.com>
2017-12-03 10:43:17 -0500

    interim edits -- need free parser

--- newbie.ml
+++ newbie.ml
@@ -1,6 +1,6 @@
 (* top-level of newbie compiler *)
 
-type actions = TOKEN | AST | SAST | (* LLVIM_IR | COMPILE | *)  DEFAULT
+type actions = TOKEN | AST | SAST | LLVIM_IR | (* COMPILE | *) DEFAULT
 
 let main () =
   let is_tag str =
@@ -12,8 +12,8 @@ let main () =
           ("-t", TOKEN)     ; (* output tokens only *)
           ("-a", AST)       ; (* output ast only*)
           ("-s", SAST)      ; (* output sast only *)
-        (* 
           ("-l", LLVIM_IR)  ; (* generate, do NOT check *)
+        (* 
           ("-c", COMPILE)     (* generate, check LLVM IR *)
          *)
       ]
@@ -44,9 +44,11 @@ let main () =
       TOKEN           -> print_endline (Scanner.string_of_tokens tokens)
     | AST             -> print_endline (Ast.string_of_program gen_ast)
     | SAST            -> print_endline (Sast.string_of_sprogram gen_sast)
-    (* | LLVIM_IR        -> print_endline (Llvm.string_of_llmodule (Codegen.translate gen_ast)) (* TODO: make gen_sast *) *)
+    | LLVIM_IR        -> print_endline (Llvm.string_of_llmodule (Codegen.translate gen_sast))
     (* | COMPILE         -> let m = Codegen.translate gen_ast in
-         Llvm_analysis.assert_valid_module m; print_string (Llvm.string_of_llmodule m) *)
-    | DEFAULT         -> print_endline (Sast.string_of_sprogram gen_sast) 
+         LLvm_analysis.assert_valid_module m; print_string (LLvm.string_of_llmodule m) *)
+    | DEFAULT         ->  print_endline (Scanner.string_of_tokens tokens) ; 
+                          print_endline (Ast.string_of_program gen_ast) ;
+                          print_endline (LLvm.string_of_llmodule (Codegen.translate gen_ast))
 
 let _ = Printexc.print main ()
