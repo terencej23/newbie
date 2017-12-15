3a46f3ee5480bd63c32042d7ae7475898871f757
Braxton Gunter <braxton.gun@gmail.com>
2017-12-15 13:09:11 -0500

    fix entrypoint

--- newbie.ml
+++ newbie.ml
@@ -43,13 +43,8 @@ let main () =
   match action with
       TOKEN           -> print_endline (Scanner.string_of_tokens tokens)
     | AST             -> print_endline (Ast.string_of_program gen_ast)
-<<<<<<< HEAD
     | SAST            -> print_endline (Sast.string_of_sprogram gen_sast)
     | LLVIM_IR        -> print_endline (Llvm.string_of_llmodule (Codegen.translate gen_sast))
-=======
-    (* | SAST            -> print_endline (Sast.string_of_program gen_sast) *)
-    (* | LLVIM_IR        -> print_endline (Llvm.string_of_llmodule (Codegen.translate gen_ast)) (* TODO: make gen_sast *) *)
->>>>>>> naming module fix -- segmentation fault in codegen
     (* | COMPILE         -> let m = Codegen.translate gen_ast in
          Llvm_analysis.assert_valid_module m; print_string (Llvm.string_of_llmodule m) *)
     | DEFAULT         ->  print_endline (Scanner.string_of_tokens tokens) ; 
