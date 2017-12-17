be48f0647c2f4755fba30fe7f422a24db3af793d
Braxton Gunter <braxton.gun@gmail.com>
2017-12-17 15:53:25 -0500

    codegen (sast)

--- newbie.ml
+++ newbie.ml
@@ -43,7 +43,7 @@ let main () =
     | AST             -> print_endline (Ast.string_of_program gen_ast)
     | SAST            -> print_endline (Sast.string_of_sprogram gen_sast)
     | LLVIM_IR        -> print_endline (Llvm.string_of_llmodule (Codegen.translate gen_sast))
-    | COMPILE         -> let m = Codegen.translate gen_ast in
+    | COMPILE         -> let m = Codegen.translate gen_sast in
          Llvm_analysis.assert_valid_module m; print_string (Llvm.string_of_llmodule m)
     | DEFAULT         -> print_endline (Scanner.string_of_tokens tokens) ; 
                          print_endline (Ast.string_of_program gen_ast) ;
